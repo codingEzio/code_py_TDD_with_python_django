4e3103dcc8bceb9903e96864501a73f7318a83a7
Ezio <assassinste@gmail.com>
2019-10-10 00:59:16 +0800

    Test whether the right template was used

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import home_page
 
@@ -8,6 +9,7 @@ from .views import home_page
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
+
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
@@ -18,3 +20,16 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith("<html>"))
         self.assertIn("<title>TODO-list</title>", html)
         self.assertTrue(html.endswith("</html>"))
+
+    def test_uses_home_template_the_manual_way(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        expected_html = render_to_string("home.html")
+
+        self.assertEqual(html, expected_html)
+
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+
+        self.assertTemplateUsed(response, "home.html")
