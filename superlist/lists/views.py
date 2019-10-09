7cd83a639fc548962499a6a39ad3e4e7d941f00a
Ezio <assassinste@gmail.com>
2019-10-10 00:46:07 +0800

    Rewritten prev HTML-in-code to template

--- superlist/lists/views.py
+++ superlist/lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("<html><title>TODO-list</title></html>")
+    return render(request=request, template_name="home.html")
